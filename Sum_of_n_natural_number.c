3b9489d004ed0604c75d202c89fd244a15d40d16
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 15:13:46 +0545

    Inline comment for clarification

--- Sum_of_n_natural_number.c
+++ Sum_of_n_natural_number.c
@@ -1,15 +1,15 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    // void in the main function is non-return type
 {
-int n,i,sum;
-sum=0;
+int n,i,sum;			//variable declaration for integer type
+sum=0;			    	//assigning value
 printf("Enter the upper limit : ");
 scanf("%d",&n);
-	for(i=0;i<=n;i++)
+	for(i=0;i<=n;i++)		//looping for printing the n whole number
 	{
 		printf("%d ",i);
 		sum=sum+i;
 	}
-printf ("\nThe sum is : %d",sum);
+printf ("\nThe sum is : %d",sum); //Printing the sum for the series
 
 }
