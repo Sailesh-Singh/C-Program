efa0d5e12285588ef9114b9a48f84bdaad3d049f
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 14:47:35 +0545

    Multi-line comment in the top for details of the authors and date of creation

--- Print_n_whole_number.c
+++ Print_n_whole_number.c
@@ -1,11 +1,19 @@
-#include<stdio.h>
-void main()
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:
+Facebook:
+linkedin:
+Date of creation:3/25/2020
+Please use this for learning purpose only.
+*/
+#include<stdio.h>		//Include basic input and output statements
+void main()			    // void in the main function is non-return type
 {
-int n,i,print;
-print=0;
+int n,i,print;			//variable declaration for integer type
+print=0;				//assigning value
 printf("Enter the upper limit : ");
 scanf("%d",&n);
-for(i=0;i<=n;i++)
+for(i=0;i<=n;i++)		//looping for printing the n whole number
 	{
 		printf("%d ",i);
 		print=print+i;
