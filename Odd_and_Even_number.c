0e5754fade7d3975c93c5fdf802b048a9375ec38
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 16:50:32 +0545

    Inline Comment for more clarification

--- Odd_and_Even_number.c
+++ Odd_and_Even_number.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    //void in the main function is non-return type
 {
-   int n;
-   printf("Enter an integer : ");
+   int n;				//variable declaration for integer type
+   printf("Enter an integer: ");
    scanf("%d",&n);
-   n%2 == 0 ? printf("Even number\n") : printf("Odd number\n");
+   n%2 == 0 ? printf("Even number\n") : printf("Odd number\n");// functional block to check the number either it is odd or even and printing the result
 }
