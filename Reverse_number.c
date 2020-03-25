4fea4ac7fbe067eeb2dafe32c82cc9fb1ac70aaf
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 16:53:37 +0545

    Multi-line comment in the top for details of the authors and date of creation

--- Reverse_number.c
+++ Reverse_number.c
@@ -1,3 +1,11 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:
+Facebook:
+linkedin:
+Date of creation:3/25/2020
+Please use this for learning purpose only.
+*/
 #include<stdio.h>		//Include basic input and output statements
 void main()			    //void in the main function is non-return type
 {
