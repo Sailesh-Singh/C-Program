f7d9d764a8e233cdd0611284f9e6242c791e53b5
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 16:30:47 +0545

    Beautify Program line of code

--- Reverse_number.c
+++ Reverse_number.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int num,rem,a=0;
+int num,rem,rev=0;
 printf("enter the number");
 scanf("%d",&num);
 while(num!=0)
-{
-rem=num%10;
-a=(a*10)+rem;
-num=num/10;
-}
-printf("the reverse is: %d",a);
+	{
+	rem=num%10;
+	rev=(rev*10)+rem;
+	num=num/10;
+	}
+printf("the reverse is: %d",rev);
 }
