042838b645d16aaa719bacdf8730d62dce04a842
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 16:38:00 +0545

    Beautify Program line of code and adding inline comment for understanding

--- Reverse_number.c
+++ Reverse_number.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    //void in the main function is non-return type
 {
-int num,rem,rev=0;
+int num,rem,rev=0;  	//variable declaration for integer type
 printf("enter the number");
 scanf("%d",&num);
 while(num!=0)
-	{
-	rem=num%10;
-	rev=(rev*10)+rem;
-	num=num/10;
-	}
-printf("the reverse is: %d",rev);
+	{							//.
+	rem=num%10;						//.
+	rev=(rev*10)+rem;					// ----> functional blocks to reverse the given number
+	num=num/10;						//.
+	}							//.
+printf("the reverse is: %d",rev);				//Printing the result
 }
