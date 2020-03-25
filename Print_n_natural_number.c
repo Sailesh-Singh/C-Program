5cf262efffd150aa9a4e6ef126d0fb1c0d43f335
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 14:57:50 +0545

    Inline comment for clarification

--- Print_n_natural_number.c
+++ Print_n_natural_number.c
@@ -1,11 +1,11 @@
-#include<stdio.h>		
-void main()			    
+#include<stdio.h>		//Include basic input and output statements
+void main()			    // void in the main function is non-return type
 {
-int n,i,print;
-print=0;
+int n,i,print;			//variable declaration for integer type
+print=0;				//assigning value
 printf("Enter the upper limit : ");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
+for(i=1;i<=n;i++)		//looping for printing the n whole number
 	{
 		printf("%d ",i);
 		print=print+i;
