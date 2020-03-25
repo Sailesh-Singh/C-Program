23e82fe5b088a869cb0a6af55d809d4b5d3eed58
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 15:46:45 +0545

    Inline Comment

--- Simple_Interest.c
+++ Simple_Interest.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    // void in the main function is non-return type
 {
-int p,t,r,SI;
+int p,t,r,SI;			//variable declaration for integer type
 printf("Enter the Principle, Time and Rate:-");
 scanf("%d%d%d",&p,&t,&r);
-SI=(p*t*r)/100;
-printf("The Simple Interest is%d",SI);
+SI=(p*t*r)/100;					//functional block to calculate the simple interest
+printf("The Simple Interest is %d",SI);//Printing the result
 }
