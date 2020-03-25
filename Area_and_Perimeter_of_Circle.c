ab0eccc4e7c92b899be0fde4826a0a3f45f2dff4
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 13:39:57 +0545

    Program Complete Area_and_Perimeter_of_Circle.c with inline comment

--- Area_and_Perimeter_of_Circle.c
+++ Area_and_Perimeter_of_Circle.c
@@ -1,4 +1,11 @@
-float r,A,P;
-
+#include<stdio.h>		//Include basic input and output statements
+int main()			    // int in the main function is return type
+{
+float r,A,P;			//float is the variable declaration for decimal type	
+printf("Enter the radius");
+scanf("%f",&r);
 A=3.14*r*r;							//functional block to calculate the area
 P=2*3.14*r;
+printf("the area is %f and he perimeter is%f",A,P);
+return 0;
+}
