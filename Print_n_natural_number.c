c5ebaa8930753f9531535f50690092414c038653
Sailesh Singh <sailesh36687@gmail.com>
2020-03-25 14:55:13 +0545

    Print_n_natural_number.c Program complete

---
+++ Print_n_natural_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>		
+void main()			    
+{
+int n,i,print;
+print=0;
+printf("Enter the upper limit : ");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+	{
+		printf("%d ",i);
+		print=print+i;
+	}
+}
