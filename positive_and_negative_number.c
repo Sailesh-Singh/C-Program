36230e08d074435dd29e71bde25ebb463b985de5
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 19:11:37 +0545

    Inline comment to understand the steps of code

--- positive_and_negative_number.c
+++ positive_and_negative_number.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    //void in the main function is non-return type
 {
-	int num;
+	int num;  			//variable declaration for integer type
 	printf("Enter the number:- ");
 	scanf("%d",&num);
-	
-	if(num>0)
+	//Starting of conditional block using if else statement
+	if(num>0) // check wether the number is greater than 0 or not
 	{
 	    printf("The number %d is positive",num);
 	}
