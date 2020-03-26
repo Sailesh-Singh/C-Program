9687af5d1871ba86bfc0d73c29a647cf3bae6056
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 02:37:04 +0545

    Date corrected

--- Matrix_subtraction.c
+++ Matrix_subtraction.c
@@ -2,7 +2,7 @@
 You can follow and look after the work done by me on following links
 Github:	  https://github.com/Sailesh-Singh
 linkedin: https://www.linkedin.com/in/saileshsingh36
-Date of creation:3/26/2020
+Date of creation:3/27/2020
 Please use this for learning purpose only.
 */
 #include<stdio.h>		//Include basic input and output statements
