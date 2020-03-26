c8d167dc9db86c3f345e67311805b17902d67300
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 18:48:05 +0545

    Inline comment for understanding the steps

--- greatest_number_2nd-method.c
+++ greatest_number_2nd-method.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
-void main ()
+#include<stdio.h>		//Include basic input and output statements
+void main ()			//void in the main function is non-return type
 {
-	int a, b, c;
+	int a, b, c;  		//variable declaration for integer type
 	printf ("enter three numbers:");
 	scanf ("%d%d%d", &a, &b, &c);
 	
+	//Starting of conditional / functional block using if else if statement	
 	if (a - b > 0 && a - c > 0)
     {
     	printf ("a is the greatest number");
