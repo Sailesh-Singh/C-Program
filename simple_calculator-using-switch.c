0b48d0e1ef8859503e901dc86ee95650edd4f9ce
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 02:45:49 +0545

    Inline comment to understand the steps of code

--- simple_calculator-using-switch.c
+++ simple_calculator-using-switch.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-int main(void)
+#include<stdio.h>		//Include basic input and output statements
+int main()		 	    //void in the main function is non-return type
 {
-    int a, b, result;
-    char op;
+    int a, b, result;	//variable declaration for integer type
+    char op;		//variable declaration for character(it may be anything)
     printf("Enter an expression: ");
     scanf("%d%c%d", &a, &op, &b);
 	//Intialization of switch statements
