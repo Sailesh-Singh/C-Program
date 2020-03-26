f4259be646fb442a79a4900e8247b7b87da3b501
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 18:55:25 +0545

    Beautify Code

--- Odd_and_Even_number-if-statement.c
+++ Odd_and_Even_number-if-statement.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int num;
-printf("enter the number");
-scanf("%d",&num);
-if(num%2==0)
-{printf("the number is even");
-}
-else
-{
-printf("the num is odd");
-}
+	int num;
+	printf("enter the number");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("the number is even");
+	}
+	else
+	{
+		printf("the num is odd");
+	}
 }
