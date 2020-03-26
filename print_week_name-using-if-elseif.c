c73d422e71a4f7f8a434d81928eacc2fd60f846c
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 03:01:22 +0545

    Multi-line comment in the top for details of the authors and date of creation

--- print_week_name-using-if-elseif.c
+++ print_week_name-using-if-elseif.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
-int main() {
-    int a;
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:3/27/2020
+Please use this for learning purpose only.
+*/
+#include <stdio.h>		//Include basic input and output statements
+int main()  	   		//int in the main function is return type
+{
+    int a;				//variable declaration for integer type
     printf("Enter integer between 1 and 7integers: ");
     scanf("%d", &a);
     if(a ==1) 
