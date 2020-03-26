db1ad6fa2f2f22fa56cbfc41f877772cc629f2ac
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 13:46:54 +0545

    Multi-line and inline comment in the top and inline respectivley for details of the authors and date of creation and the details of program and its functioning

--- profit_and_percentage.c
+++ profit_and_percentage.c
@@ -1,10 +1,17 @@
-#include<stdio.h>
-main()
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:3/26/2020
+Please use this for learning purpose only.
+*/
+#include<stdio.h>		//Include basic input and output statements
+void main()			    //void in the main function is non-return type
 {
-int sp,cp,p,per;
-printf("Enter the sp and cp");
-scanf("%d%d",&sp,&cp);
-p=sp-cp;
-per=(p*100)/cp;
-printf("The profit is %d and percentage is %d",p,per);
+	int sp,cp,p,per;  	//variable declaration for integer type
+	printf("Enter the sp and cp");
+	scanf("%d%d",&sp,&cp);
+	p=sp-cp;					//functional blocks to calculate profit amount
+	per=(p*100)/cp;				//functional blocks to calculate profit percent
+	printf("The profit is %d and percentage is %d",p,per);  //Printing result
 }
