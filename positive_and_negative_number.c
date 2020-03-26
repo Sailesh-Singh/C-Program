fc0920dcf20306b6ba110b6deb33a41d4294ab9d
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 19:07:42 +0545

    Program complete

---
+++ positive_and_negative_number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int num;
+printf("Enter the number:- ");
+scanf("%d",&num);
+if(num>0)
+{
+    printf("The number %d is positive",num);
+}
+else
+    {
+    printf("The number %d is negative",num);
+}
+}
+
+
