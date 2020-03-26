55e6d4050a8d0a1741f3e02fbba04e26afa9e567
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 09:28:54 +0545

    Inline comment for support and clarity

--- max_num_betwn_n_num.c
+++ max_num_betwn_n_num.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    //void in the main function is non-return type
 {
-int value[5],i,max,n;
+int value[5],i,max,n;  	//variable declaration for integer type
 printf("How many number you wan to check?: ");
 scanf("%d",&n);
 	for(i=1; i<=n; i++)
@@ -9,10 +9,10 @@ scanf("%d",&n);
 		printf("enter the number");
 		scanf("%d",&value[i]);
 	}
-	for(i=0;i<=n;i++)
-	{
-		if(max<value[i])
-		max= value[i];
-	}
-printf("the maximum number is %d",max);
+	for(i=0;i<=n;i++)			//.
+	{								//.
+		if(max<value[i])				// ----> functional blocks to check which number is maximum
+		max= value[i];				//.
+	}							//.
+printf("the maximum number is %d",max);				//Printing the result
 }
