8410f5e114d9bc262c917c8bd538fcbb25841f2b
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 14:33:54 +0545

    Program complete

---
+++ Root_of_Quadratic_eqn.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    double a, b, c, d, r1, r2, rp, ip;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+    d = b*b - 4*a*c;
+    if (d > 0) 
+	{
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
+        printf("r1 = %.2lf and r2 = %.2lf", r1, r2);
+    }
+    else if (d == 0) 
+	{
+        r1 = r2 = -b / (2*a);
+        printf("r1 = r2 = %.2lf;", r1);
+    }
+    else {
+        rp = -b / (2*a);
+        ip = sqrt(-d) / (2*a);
+        printf("r1 = %.2lf+%.2lfi and r2 = %.2f-%.2fi", rp, ip, rp, ip);
+    }
+    return 0;
+}
+
+
+
+
