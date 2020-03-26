ae76d5976c9bac90d2b4fd38f5a4bffa4a2aea11
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 13:52:49 +0545

    Program comlete

---
+++ area_between_concentric_circles.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+float r,R,a,A;
+printf("enter the first radius");
+scanf("%f",&r);
+printf("enter the second radius");
+scanf("%f",&R);
+a=3.14*r*r;
+A=3.14*R*R;
+printf("the area between the concentric circle is %f%f",a,A);
+}
