e94d75c6c15e825a68f2fb4d31f35e031ce4f9ce
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 13:35:37 +0545

    Program complete with inline comments

--- product_of_number.c
+++ product_of_number.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-main()
+#include<stdio.h>		//Include basic input and output statements
+void main()			    	//void in the main function is non-return type
 {
-	int a,b,c;
+	int a,b,c;  					//variable declaration for integer type
 	printf("enter two numbers");
 	scanf("%d%d",&a,&b);
-	c=a*b;
-	printf("the product is %d",c);
+	c=a*b;							//functional blocks to multiply two number
+	printf("the product is %d",c);	//Printing the result
 }
