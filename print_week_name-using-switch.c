e45c6b6232994e47d785e31f856d977a8adfac2f
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 02:52:58 +0545

    Inline comment to understand the steps of code and program is complete

---
+++ print_week_name-using-switch.c
@@ -0,0 +1,35 @@
+#include <stdio.h>		//Include basic input and output statements
+int main()	 	   		//void in the main function is non-return type
+{
+    int week;			//variable declaration for integer type
+    printf("Enter week number(1-7): ");
+    scanf("%d", &week);
+// Intializing switch statement
+    switch(week)
+    {
+        case 1:
+            printf("Monday");
+            break;
+        case 2:
+            printf("Tuesday");
+            break;
+        case 3:
+            printf("Wednesday");
+            break;
+        case 4:
+            printf("Thursday");
+            break;
+        case 5:
+            printf("Friday");
+            break;
+        case 6:
+            printf("Saturday");
+            break;
+        case 7:
+            printf("Sunday");
+            break;
+        default:
+            printf("Invalid input! Please enter week number between 1-7.");
+    }
+    return 0;
+}
