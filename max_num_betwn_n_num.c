7155009f42de7af28d10ea82ecd71b7184b6ddf0
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 09:24:42 +0545

    'n' variable is intialize instead of direct value

--- max_num_betwn_n_num.c
+++ max_num_betwn_n_num.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int value[5],i,max;
-	for(i=0; i<=5; i++)
+int value[5],i,max,n;
+printf("How many number you wan to check?: ");
+scanf("%d",&n);
+	for(i=1; i<=n; i++)
 	{
 		printf("enter the number");
 		scanf("%d",&value[i]);
 	}
-	for(i=0;i<=5;i++)
+	for(i=0;i<=n;i++)
 	{
 		if(max<value[i])
 		max= value[i];
