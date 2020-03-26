73417aea9b4790f410f7b136773798af28437cd7
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 18:49:02 +0545

    Multi-line comment in the top for details of the authors and date of creation

--- greatest_number_2nd-method.c
+++ greatest_number_2nd-method.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:3/26/2020
+Please use this for learning purpose only.
+*/
 #include<stdio.h>		//Include basic input and output statements
 void main ()			//void in the main function is non-return type
 {
