e2d0486330100391a7e5895991ccf7932187438d
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 19:33:39 +0545

    Reduce the unnecessary lines of code.

---
+++ Matrix_addition.c
@@ -0,0 +1,33 @@
+#include<stdio.h>		//Include basic input and output statements
+int main() 		    	//int in the main function is return type
+{
+    int n, m, c, d, first[10][10], second[10][10], sum[10][10], diff[10][10];  //variable declaration for integer type also using array
+    printf("\nEnter the number of rows and columns of the first matrix\n");
+    scanf("%d%d", &m, &n);
+
+    printf("Enter the %d elements of the first matrix \n", m*n); 	//first elements of matrix
+    for(c = 0; c < m; c++)   				// to iterate the rows
+        for(d = 0; d < n; d++)   			// to iterate the columns
+            scanf("%d", &first[c][d]);
+    printf("\nEnter the %d elements of the second matrix\n", m*n);	//second elements of matrix
+    for(c = 0; c < m; c++)   				// to iterate the rows
+        for(d = 0; d < n; d++)   			// to iterate the columns
+            scanf("%d", &second[c][d]);
+
+    //finding the SUM of the two matrices
+    for(c = 0; c < m; c++)
+        for(d = 0; d < n; d++)
+            sum[c][d] = first[c][d] + second[c][d];
+
+    // printing the elements of the sum matrix
+    printf("\n\nThe sum of the two entered matrices is: \n\n");
+    for(c = 0; c < m; c++)
+    {
+        for(d = 0; d < n; d++)
+        {
+            printf("%d\t", sum[c][d]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
