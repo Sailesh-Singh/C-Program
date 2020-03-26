6dabd3a1bc0933e337bc1b897e91a04ffe602cfe
Sailesh Singh <sailesh36687@gmail.com>
2020-03-26 09:21:18 +0545

    Beautify code

--- max_num_betwn_n_num.c
+++ max_num_betwn_n_num.c
@@ -2,15 +2,15 @@
 void main()
 {
 int value[5],i,max;
-for(i=0; i<=5; i++)
-{
-printf("enter the number");
-scanf("%d",&value[i]);
-}
-for(i=0;i<=5;i++)
-{
-if(max<value[i])
-max= value[i];
-}
+	for(i=0; i<=5; i++)
+	{
+		printf("enter the number");
+		scanf("%d",&value[i]);
+	}
+	for(i=0;i<=5;i++)
+	{
+		if(max<value[i])
+		max= value[i];
+	}
 printf("the maximum number is %d",max);
 }
