0a2befd89dc612a29f291fc46c72fdc6d69c18b7
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 16:27:36 +0545

    All else case remove and make program short and program work correct

--- Check_types_of_triangle.c
+++ Check_types_of_triangle.c
@@ -12,11 +12,11 @@ void main()
 		if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b)
 			printf("\nThe triangle is right angle triangle");
 		
+		else if(a==b && b==c && a==c)
+			printf("\nThe triangle is equilateral");	
 			
 		else if(a==b || b==c || a==c)
 			printf("\nThe triangle is isosceles");		
-		else if(a==b && b==c && a==c)
-			printf("\nThe triangle is equilateral");	
 	}
 } 
 //It is necessary to keep the code in same order
