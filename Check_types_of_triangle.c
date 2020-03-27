0deee219f1f7d7db79184b68cf438ca186fbf977
Sailesh Singh <sailesh36687@gmail.com>
2020-03-27 16:13:03 +0545

    comment all else case

--- Check_types_of_triangle.c
+++ Check_types_of_triangle.c
@@ -6,34 +6,37 @@ void main()
 	printf("enter the three side of triangle:\t");
 	scanf("%d%d%d",&a,&b,&c);
 	//conditions to check the types of triangle 
-	if(a<(b+c) || b<(a+b) || c<(a+b))
+if(a<(b+c) || b<(a+b) || c<(a+b))
+{
+	
 		printf("The triangle is valid");
-	else
-	{
-		printf("The triangle is not valid");
-	}
-	else if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b)
+}
+//	else
+//	{
+//		printf("The triangle is not valid");
+//	}	
+else if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b)
 	{
 		printf("The triangle is right angle triangle");
 	}
-	else
-	{
-	    printf("The triangle is not right angle");
-	}
+//	else
+//	{
+//	    printf("The triangle is not right angle");
+//	}
 	else if(a==b || b==c || a==c)
 	{
 		printf("The triangle is isosceles");
 	}
-	else
-	{
-	    printf("The triangle is not isosceles");
-	}
+//	else
+//	{
+//	    printf("The triangle is not isosceles");
+//	}
 	else if(a==b && b==c && a==c)
 	{
 		printf("The triangle is equilateral");
 	}
-	else
-	{
-	    printf("The triangle is not equilateral");
-	}
+//	else
+//	{
+//	    printf("The triangle is not equilateral");
+//	}
 }
