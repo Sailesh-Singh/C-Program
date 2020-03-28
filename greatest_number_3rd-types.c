325291c2346d2c1225885758fb13255ab70ba6d7
Sailesh Singh <sailesh36687@gmail.com>
2020-03-28 13:37:51 +0545

    Inline Comment

--- greatest_number_3rd-types.c
+++ greatest_number_3rd-types.c
@@ -1,10 +1,11 @@
 
-#include <stdio.h>
-void main()
+#include <stdio.h>		//Include basic input and output statements
+void main()		    	//int in the main function is return type
 {
-	int n1,n2,n3;
+	int n1,n2,n3;  		//variable declaration for integer type
 	printf("\nEnter 3 numbers: ");
 	scanf("%d %d %d",&n1,&n2,&n3);
+//	Intializing of functional block to determine greatest number
 	if(n1>n2)
 		{
 		if(n1>n3)
