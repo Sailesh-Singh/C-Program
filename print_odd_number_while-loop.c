18a7f474b73cac0f9b3e0edcbdaf90c577aec770
Sailesh Singh <sailesh36687@gmail.com>
2020-03-28 14:01:34 +0545

    Inline Comment

--- print_odd_number_while-loop.c
+++ print_odd_number_while-loop.c
@@ -1,12 +1,13 @@
 
 
-#include <stdio.h>
-int main()
+#include <stdio.h>		//Include basic input and output statements
+int main()	    		//int in the main function is return type
 {
-    int i, n;
+    int i, n; 			//variable declaration for integer type
     scanf("%d", &n);
     printf("Print all odd numbers from %d to n :",n);
-    i=1;
+    i=1;       //Value assigning
+//    Begining of logical and functional block using while loop
     while(i<=n)
     {
         printf("%d\n", i);
