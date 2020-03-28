980c6d56c73215ffae47a51ced8fa175312ede09
Sailesh Singh <sailesh36687@gmail.com>
2020-03-28 14:52:51 +0545

    Inline Comment

--- sum_of_n_square-number.c
+++ sum_of_n_square-number.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-void main()
+#include <stdio.h>		//Include basic input and output statements
+void main()		 	   	//void in the main function is non-return type
 {
-    int i, n,sum=0;
+    int i, n,sum=0;		//variable declaration for integer type
     printf("Enter the number: ");
     scanf("%d", &n);
-    i=1;
+    i=1;				//assigning value
     while(i<=n)
     {
          sum=sum+i*i;
-        i++;
+        i++;			//counter
     } 
 	printf("Sum of %d square numbers =%d\n",n, sum);
 }
