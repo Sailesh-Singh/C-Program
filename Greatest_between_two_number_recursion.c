9d670807e8c5ff59d32d4f812edbdf38956bf484
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 14:16:38 +0545

    Make Multiline comment in the top of code for author details and contact links with date of creation

--- Greatest_between_two_number_recursion.c
+++ Greatest_between_two_number_recursion.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/21/2020
+Please use this for learning purpose only.
+*/
 //Program to find greatest number between two number using recursion; with argument and without return type
 #include<stdio.h>			//Include basic input and output statements
 // Intializing sub-function	with argument(int a, int b) and without returning type
