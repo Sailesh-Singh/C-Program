74641c66beafadbc76a20779802c1c80f7fa15ae
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 14:15:44 +0545

    Inline comment to make code steps easy to understand

--- Greatest_between_two_number_recursion.c
+++ Greatest_between_two_number_recursion.c
@@ -1,20 +1,20 @@
 //Program to find greatest number between two number using recursion; with argument and without return type
-#include<stdio.h>
+#include<stdio.h>			//Include basic input and output statements
+// Intializing sub-function	with argument(int a, int b) and without returning type
 void greater(int a, int b)
 {
-	int largest;
-	if(a>b)
+	int largest;    		//variable declaration for integer type
+	if(a>b)					//IF_ELSE to check the conddition
 		largest=a;
 	else
 		largest=b;
-	printf("%d is largest\n",largest);
+	printf("%d is largest\n",largest);	//Printing the result
 }
 
-int main()
+int main()	//int in the main function is return type
 {
-	int x,y,result;
+	int x,y,result;    		//variable declaration for integer type
 	printf("Enter the number to be checked: ");
 	scanf("%d%d",&x,&y);
-	greater(x,y);
-
+	greater(x,y);			//Returning the value of (x,y) to (int a, int b)	
 }
