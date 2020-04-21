4ef51e59e6a96b49af2585f63cc55301ce46c2eb
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 15:57:38 +0545

    Make Multiline comment in the top of code for author details and contact links with date of creation

--- Print_n_whole_number_while-loop.c
+++ Print_n_whole_number_while-loop.c
@@ -1,4 +1,10 @@
-
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/21/2020
+Please use this for learning purpose only.
+*/
 //Program to print the 'n' whole number using while loop statement
 #include<stdio.h>	
 void main()			
