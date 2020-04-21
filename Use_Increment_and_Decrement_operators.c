ceaf57973d25ea54874f69080949cd6c345fef58
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 19:37:06 +0545

    Inline comment to make code steps easy to understand with general variable solution instance

--- Use_Increment_and_Decrement_operators.c
+++ Use_Increment_and_Decrement_operators.c
@@ -1,16 +1,16 @@
 //Program to use and analyse all the increment and decrement operators
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
-	int x,y;
+	int x,y;			//variable declaration for integer type
 	printf("Enter the number: ");
 	scanf("%d",&x);
-	y=x++;
+	y=x++;				//Post increment. First it print y=x, then add 1
+	printf("y=%d\n",y);	
+	y=x--;				//Post descrement.Here x=(x+1). First it print y=x+1, then subtract 1 from x.
 	printf("y=%d\n",y);
-	y=x--;
+	y=--x;				//Pre increment. Here x=((x+1)-1) i.e x=x. First it subtract 1 from x then print it i.e y=x-1 
 	printf("y=%d\n",y);
-	y=--x;
-	printf("y=%d\n",y);
-	y=++x;
+	y=++x;				//Post decrement.Here x=x-1. First it add 1 to x then print it i.e y=(x-1)+1  =>y=x
 	printf("y=%d\n",y);
 }
