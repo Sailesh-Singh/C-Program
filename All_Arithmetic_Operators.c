5a2ed191cc55ce466208fc4fd16575a0aad7494b
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 16:42:43 +0545

    Inline comment to make code steps easy to understand

--- All_Arithmetic_Operators.c
+++ All_Arithmetic_Operators.c
@@ -1,18 +1,18 @@
 //Program to use all the arithmetic operators using any two numbers
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
-    int a,b,sum,diff,product,div,rem;
+    int a,b,sum,diff,product,div,rem;	//variable declaration for integer type
     printf("Enter the two numbers: ");
     scanf("%d%d",&a,&b);
-    sum=a+b;
-    diff=a-b;
-    product=a*b;
-    div=a/b;
-    rem=a%b;
-    printf("a+b=%d\n",sum);
-    printf("a-b=%d\n",diff);
-    printf("a*b=%d\n",product);
-    printf("a%b=%d",rem);
-    printf("a/b=%d\n",div);
+    sum=a+b;				//.
+    diff=a-b;					//.	
+    product=a*b;					//.--->Using the arithmetic operators and storing its value to its respective variable
+    div=a/b;					//.
+    rem=a%b;				//.
+    printf("a+b=%d\n",sum);///////////
+    printf("a-b=%d\n",diff);		//
+    printf("a*b=%d\n",product);		//::--> Printing the result of used airthmetic operators
+    printf("a%b=%d",rem);			//
+    printf("a/b=%d\n",div);///////////
 }
