7b625998f8428df91bd66d31f50f0926a2baffc8
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 11:31:06 +0545

    Inline comment to understand the steps

--- Odd_and_Even_number_recursion.c
+++ Odd_and_Even_number_recursion.c
@@ -6,15 +6,16 @@ Date of creation:4/21/2020
 Please use this for learning purpose only.
 */
 //Program to check wether the given number is Odd or Even using recursion and using ternary operator
-#include <stdio.h>
-int main()
+#include <stdio.h>		//Include basic input and output statements
+int main()		        //int in the main function is return type
 {
-    int n;
+    int n; 	            //variable declaration for integer type
     printf("Enter a positive integer: ");
     scanf("%d",&n);
-    return oddeven(n);
+    return oddeven(n);	//Calling sub-function to jump to sub-function
 }
-void oddeven(int a) 
-{
-    a%2==0?printf("its even"):printf("Its Odd");
-}
+// Intialization of sub-function
+void oddeven(int a)                        
+{													
+    a%2==0?printf("its even"):printf("Its Odd"); // Conditional operator to check the condition for odd and even  
+}										
