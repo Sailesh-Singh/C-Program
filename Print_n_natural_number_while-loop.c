6394b2dd5c306bdb36e8777f8b95be7c34ce3bda
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 15:42:02 +0545

    Make Multiline comment in the top of code for author details and contact links with date of creation

--- Print_n_natural_number_while-loop.c
+++ Print_n_natural_number_while-loop.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/21/2020
+Please use this for learning purpose only.
+*/
 //Program to print the 'n' natural number using while loop statement
 #include<stdio.h>
 void main()
