f3d1031621c10393e31381a840d5d4c1ea5ae075
Sailesh Singh <sailesh36687@gmail.com>
2020-04-21 13:36:35 +0545

    Make Multiline comment in the top of code for author details and contact links with date of creation

--- Reverse_number_recursion_return-type.c
+++ Reverse_number_recursion_return-type.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/21/2020
+Please use this for learning purpose only.
+*/
 //Program to reverse the number using using recursion;return type
 #include<stdio.h>
 int reverse(int n)
