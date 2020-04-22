da18bf1ca65d7f1c05b0aa03b0250d2d04f22951
Sailesh Singh <sailesh36687@gmail.com>
2020-04-22 22:49:42 +0545

    Inline comment to understand the steps of program

--- Check_a_is_multiple_of_b_or_not.c
+++ Check_a_is_multiple_of_b_or_not.c
@@ -1,17 +1,17 @@
 //Program to check the number a is multiple of b or not
-#include<stdio.h>
-void main()
+#include<stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
-	int a,b;
+	int a,b;			//variable declaration for integer type 
 	printf("Enter the divisor and dividend: ");
 	scanf("%d%d",&a,&b);
-	if(a%b==0)
+	if(a%b==0)			//"==" is checker sign that check a%b is 0 or not
 	{
-		printf("The number %d is multiple of %d\n",a,b);
-		printf("i.e %d x %d = %d",a/b,b,a);
+		printf("The number %d is multiple of %d\n",a,b);//Print this line if a%b is 0
+		printf("i.e %d x %d = %d",a/b,b,a);				//Print this line if a%b is 0
 	}
 	else
 	{
-		printf("The number %d is not multiple of %d",a,b);
+		printf("The number %d is not multiple of %d",a,b);//Print this line if a%b is not 0 
 	}
 }
