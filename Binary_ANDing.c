c5456ca653abd62b6242f6e9b0aec232b4ce9eb5
Sailesh Singh <sailesh36687@gmail.com>
2020-04-22 19:59:37 +0545

    Inline comment to understand the steps of program

--- Binary_ANDing.c
+++ Binary_ANDing.c
@@ -1,10 +1,10 @@
 //Program to AND two numbers
-#include <stdio.h>
-int main()
+#include <stdio.h>		//Include basic input and output statements
+int main()				//int in the main function is return type
 {
-    int a,b;
+    int a,b;			//variable declaration for integer type 
     printf("Enter two number: ");
-    scanf("%d%d",&a,&b);
-    printf("ANDing of %d and %d = %d",a,b,a&b);
+    scanf("%d%d",&a,&b);// Scan or take input entered by user
+    printf("ANDing of %d and %d = %d",a,b,a&b);//Printing the result
     return 0;
 }
