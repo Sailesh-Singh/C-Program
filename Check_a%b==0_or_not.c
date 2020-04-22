1338ad3d012380b6f633ee38401047d7018df991
Sailesh Singh <sailesh36687@gmail.com>
2020-04-22 22:24:51 +0545

    Multiline comment in the top of code for author details and contact links with date of creation

--- Check_a%b==0_or_not.c
+++ Check_a%b==0_or_not.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/22/2020
+Please use this for learning purpose only.
+*/
 //Program to Check wether divisor is exactly divisible by dividend or not
 #include<stdio.h>		//Include basic input and output statements
 void main()				//void in the main function is non-return type
