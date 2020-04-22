4550b9698c1d34d1cda94a1bfee396fc7611c2d5
Sailesh Singh <sailesh36687@gmail.com>
2020-04-22 19:54:07 +0545

    Program code "Program to AND two numbers" complete

---
+++ Binary_ANDing.c
@@ -0,0 +1,10 @@
+//Program to AND two numbers
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two number: ");
+    scanf("%d%d",&a,&b);
+    printf("ANDing of %d and %d = %d",a,b,a&b);
+    return 0;
+}
