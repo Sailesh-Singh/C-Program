a882e0b9b12a9b6c32755ddaa8bc087d09bdb22c
Sailesh Singh <sailesh36687@gmail.com>
2020-04-23 16:23:34 +0545

    Multiline comment in the top of code for author details and contact links with date of creation

--- Sizeof_keywords.c
+++ Sizeof_keywords.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:4/23/2020
+Please use this for learning purpose only.
+*/
 //Program to calculate the sizeof keywords in bytes
 #include<stdio.h>		//Include basic input and output statements
 void main()		 	   	//void in the main function is non-return type 
