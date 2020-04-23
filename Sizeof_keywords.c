fb605c2b0fec96d26a71ceccdfd2a96f371965fe
Sailesh Singh <sailesh36687@gmail.com>
2020-04-23 16:05:20 +0545

    Program code "Program to calculate the sizeof keywords in bytes" complete

---
+++ Sizeof_keywords.c
@@ -0,0 +1,74 @@
+//Program to calculate the sizeof keywords in bytes
+#include<stdio.h>
+void main() 
+{
+////	auto autoType;
+////	break breakType;
+////	case caseType;
+	char charType;
+////	const constType;
+////	continue continueType;
+////	default defaultType;
+////	do doType;
+	double doubleType;
+////	else elseType;
+////	enum enumType;
+////	extern externType;
+	float floatType;
+////	for forType;
+////	goto gotoType;
+////	if ifType;
+	int intType;
+////	long longType;
+////	register registerType;
+////	return returnType;
+////	short shortType;
+////	signed signedType;
+////	sizeof sizeofType;
+////	static staticType;
+////	struct structType;
+////	switch switchType;
+////	typedef typedefType;
+////	union unionType;
+////	unsigned unsignedType;
+////	void voidType;
+////	volatile volatileType;
+////	while whileType;
+    printf("Size of int: %d bytes\n", sizeof(intType));
+    printf("Size of float: %ld bytes\n", sizeof(floatType));
+    printf("Size of double: %ld bytes\n", sizeof(doubleType));
+    printf("Size of char: %ld byte\n", sizeof(charType));
+//printf("Size of auto: %d bytes\n", sizeof(autoType));
+//printf("Size of break: %d bytes\n", sizeof(breakType));
+//printf("Size of case: %d bytes\n", sizeof(caseType));
+//printf("Size of char: %d bytes\n", sizeof(charType));
+//printf("Size of const: %d bytes\n", sizeof(constType));
+//printf("Size of continue: %d bytes\n", sizeof(continueType));
+//printf("Size of default: %d bytes\n", sizeof(defaultType));
+//printf("Size of do: %d bytes\n", sizeof(doType));
+//printf("Size of double: %d bytes\n", sizeof(doubleType));
+//printf("Size of else: %d bytesbytes\n", sizeof(externType));
+//printf("Size of float: %d \n", sizeof(elseType));
+//printf("Size of enum: %d bytes\n", sizeof(enumType));
+//printf("Size of extern: %d bytes\n", sizeof(floatType));
+//printf("Size of for: %d bytes\n", sizeof(forType));
+//printf("Size of goto: %d bytes\n", sizeof(gotoType));
+//printf("Size of if: %d bytes\n", sizeof(ifType));
+//printf("Size of int: %d bytes\n", sizeof(intType));
+//printf("Size of long: %d bytes\n", sizeof(longType));
+//printf("Size of register: %d bytes\n", sizeof(registerType));
+//printf("Size of return: %d bytes\n", sizeof(returnType));
+//printf("Size of short: %d bytes\n", sizeof(shortType));
+//printf("Size of signed: %d bytes\n", sizeof(signedType));
+//printf("Size of sizeof: %d bytes\n", sizeof(sizeofType));
+//printf("Size of static: %d bytes\n", sizeof(staticType));
+//printf("Size of struct: %d bytes\n", sizeof(structType));
+//printf("Size of switch: %d bytes\n", sizeof(switchType));
+//printf("Size of typedef: %d bytes\n", sizeof(typedefType));
+//printf("Size of union: %d bytes\n", sizeof(unionType));
+//printf("Size of unsigned: %d bytes\n", sizeof(unsignedType));
+//printf("Size of void: %d bytes\n", sizeof(voidType));
+//printf("Size of volatile: %d bytes\n", sizeof(volatileType));
+//printf("Size of while: %d bytes\n", sizeof(whileType));
+}
+
