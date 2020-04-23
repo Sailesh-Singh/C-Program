ea6f982db562aa130b622dc5cb7b636eae561fca
Sailesh Singh <sailesh36687@gmail.com>
2020-04-23 12:40:23 +0545

    Inline comments to understand the steps of program

--- Solution_of_Pair_of_St._line.c
+++ Solution_of_Pair_of_St._line.c
@@ -1,20 +1,21 @@
-#include<stdio.h>
-void main()
+//Program to find the solution of pairs of straight lines
+#include<stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
     float a,b,c,d,m,n,x1,x2;
     printf("Enter the value of a,b and m of ax1+bx2=m:\t");
-    scanf("%fgit%f%f",&a,&b,&m);
+    scanf("%f%f%f",&a,&b,&m);//Scan value for first line
     printf("Enter the value of c,d and n of cx1+dx2=n:\t");
-    scanf("%f%f%f",&c,&d,&n);
+    scanf("%f%f%f",&c,&d,&n);//Scan value for second line
     int denominator=a*d-c*b;
-    if(denominator==0)
+    if(denominator==0)//Condition true then further calculation cannot be done, value will be infinite
     {
         printf("The denominator is zero");
     }
     else
 	{
-        x1=(m*d-b*n)/denominator;
+        x1=(m*d-b*n)/denominator; 
         x2=(n*a-m*c)/denominator;
-        printf("The solution of lines:\nX1= %.2f\t X2= %.2f",x1,x2);
+        printf("The solution of lines:\nX1= %.2f\t X2= %.2f",x1,x2); //Printiing the float value of X1 and X2 up to 2 decimal places
     }
 }
