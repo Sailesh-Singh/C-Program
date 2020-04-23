f23976bb07fc0e835e96e2c2022f29af79ef7af0
Sailesh Singh <sailesh36687@gmail.com>
2020-04-23 12:58:09 +0545

    Inline comments to understand the steps of program

--- Print_statements_for_n_times.c
+++ Print_statements_for_n_times.c
@@ -1,12 +1,14 @@
-
-#include <stdio.h>
-void main()
+//Program to print statements for n times using for loop
+#include <stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
-	int i;
-	for(i=0; i<10; i++)
+	int i, n;				//variable declaration for integer type
+	printf("How many times you want to print the statements?\n=> ");
+	scanf("%d",&n); 		//Asking the number of times we want to run loop
+	for(i=0; i<n; i++)
 	{
 	printf("\n My name is Sailesh Singh.");
-	printf("\n I live in Nepal.");
+	printf("\n I live in Nepal.\n");
 	}
 }
 
