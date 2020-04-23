1c29f94f901421706346e3c2fdadc928b9b86223
Sailesh Singh <sailesh36687@gmail.com>
2020-04-23 14:46:53 +0545

    Inline comments to understand the steps of program

--- Sum_of_n_square-number_2nd-method.c
+++ Sum_of_n_square-number_2nd-method.c
@@ -1,20 +1,19 @@
 //Using do while loop print and find the sum of square number up to n
-#include <stdio.h>
-int main()
+#include <stdio.h>		//Include basic input and output statements
+void main()	     	   	//void in the main function is non-return type
 {
-    int n, i, s, sum=0;
+    int n, i, s, sum=0;	//variable declaration for integer type
     do
     {
         printf("Enter a positive integer: ");
         scanf("%d", &n);
-    } while (n <= 0);
+    } while (n <= 0);	//Check the integer is positive else it will repeat the do loop untill positive number is entered
     for (i = 1; i <= n; ++i)
     {
     s=i*i;	
     sum=sum+s;
-    printf("%d\n",s);
+    printf("%d\n",s);	//Print the square on number in new line
     }
-    printf("Sum=%d",sum);
-    return 0;
+    printf("Sum=%d",sum);//Print the sum of all the square number
 }
 
