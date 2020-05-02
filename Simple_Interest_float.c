6d706202c92f506e4ef286eede1a0046dcf8cbb7
Sailesh Singh <sailesh36687@gmail.com>
2020-05-02 18:16:06 +0545

    Inline comments to understand the steps of program

--- Simple_Interest_float.c
+++ Simple_Interest_float.c
@@ -1,15 +1,16 @@
 //Program to calculate the Simple Interest using float
-#include <stdio.h>
-void main()
+#include <stdio.h>		//Include basic input and output statements
+void main()			    // void in the main function is non-return type
 {
-	int p,t,r;
-    float I;
-    printf("Enter principal: ");
-    scanf("%d",&p);
+	int p,t,r;			//variable declaration for integer type
+    float I;			//variable declaration for float type i.e fractional type
+    printf("Enter principle: ");
+    scanf("%d",&p);		//Scaning the principle from the user
     printf("Enter Time: ");
-    scanf("%d",&t);
+    scanf("%d",&t);		//Scaning the time from the user
     printf("Enter Rate: ");
-    scanf("%d",&r);
-    I=(p*t*r)/100;
-    printf("The intrest is %f",I);
+    scanf("%d",&r);		//Scaning the rate from the user
+    I=(p*t*r)/100;				//functional statement to calculate the simple interest
+    printf("The intrest is %f",I);//Printing the result
 }
+
