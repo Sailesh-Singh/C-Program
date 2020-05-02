e8a17f98659f2daa0450c0493c869167ab14158d
Sailesh Singh <sailesh36687@gmail.com>
2020-05-02 18:18:10 +0545

    Multiline comment in the top of code for author details and contact links with date of creation

--- Simple_Interest_float.c
+++ Simple_Interest_float.c
@@ -1,3 +1,10 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:5/02/2020
+Please use this for learning purpose only.
+*/
 //Program to calculate the Simple Interest using float
 #include <stdio.h>		//Include basic input and output statements
 void main()			    // void in the main function is non-return type
