ffdd2a553e0a024accfd39baadcd0faf9b13b917
Sailesh Singh <sailesh36687@gmail.com>
2020-05-10 11:19:11 +0545

    Inline comments to understand the each steps of program

--- Swap_using_third_variable.c
+++ Swap_using_third_variable.c
@@ -1,15 +1,15 @@
 //Program to Swap two number using third variable
-#include <stdio.h>
-void main()
+#include <stdio.h>		//Include basic input and output statements
+void main()				//void in the main function is non-return type
 {
-	int a,b,c;
+	int a,b,c;			//variable declaration for integer type
 	printf("Enter two numbers: \n");
-	scanf("%d%d",&a,&b);
+	scanf("%d%d",&a,&b);		//Scaning the number 'a' and 'b' from the user
 	printf("\n\nBefore Swaping\n");
-	printf("A=%d B=%d \n",a,b);
-	c=a+b;
-	a=c-a;
-	b=c-b;
+	printf("A=%d B=%d \n",a,b);	//Printing the number before swaping
+	c=a+b;					//.
+	a=c-a;						//----->functional block to swap two number using third variable
+	b=c-b;					//.
 	printf("\n\nAfter Swaping\n");
-	printf("A=%d B=%d \n",a,b);
+	printf("A=%d B=%d \n",a,b);	//Printing the number after swaping
 }
