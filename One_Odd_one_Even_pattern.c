4e5803ffae67879b2c9a44b8f5d94dc9521f75ac
Sailesh Singh <sailesh36687@gmail.com>
2020-05-12 18:58:42 +0545

    Inline comments to understand the each steps of program.

--- One_Odd_one_Even_pattern.c
+++ One_Odd_one_Even_pattern.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-void odd()
+//Program to print odd and even alternatively
+#include<stdio.h>		//Include basic input and output statements
+void odd()				//void in the sub-function is non-return type
 {
-	int i=1,n, count=1;
+	int i=1,n, count=1;// variable declaration for integer type
     printf("Enter n:");
     scanf("%d",&n);
     while (i<=n)
@@ -11,7 +12,8 @@ void odd()
         count=count+2;
         i++;
         }
-        {printf("%d\n",count);
+        {
+		printf("%d\n",count);
         count=count+2;
         i++;
         }
@@ -19,7 +21,7 @@ void odd()
         i++;
     }
 }
-void main()
+void main()				//void in the main function is non-return type
 {
-    odd();
+    odd();		//Odd sub-function is called
 }
