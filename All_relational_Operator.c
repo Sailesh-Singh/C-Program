c1c55c07b60fcdfc4e2c5fe82992dc024371681c
Sailesh Singh <sailesh36687@gmail.com>
2020-05-12 12:03:57 +0545

    Inline comments to understand the each steps of program

--- All_relational_Operator.c
+++ All_relational_Operator.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-int main()
+//WAP to use all relational operator
+#include<stdio.h>		//Include basic input and output statements
+int main()				//int in the main function is return type
 {
-	int a,b;
+	int a,b;			//variable declaration for integer type
 	printf("Enter the two numbers:\t");
-	scanf("%d%d",&a,&b);
+	scanf("%d%d",&a,&b);//scaning two numbers from the user
+	//Conditional check using if
 	if (a!=b)
 	{
 	printf("%d is not equal to %d\n",a,b);
+		//if conditional check inside the if condition
 		if(a>b)
 		{
 			printf("%d is greater than %d i.e (%d>%d)\n",a,b,a,b);
