fe0d508dd00b28ecc430ccaed2bde2ceed71aff9
Sailesh Singh <sailesh36687@gmail.com>
2020-05-24 23:56:09 +0545

    Inline comments to understand the each steps of program.

--- Bitwise_AND.c
+++ Bitwise_AND.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
-int main(void)
+//Program to AND two unsigned integers
+#include <stdio.h>		//Include basic input and output statements
+int main(void)			//int in the main function is return type
 {
-unsigned int a, b;
-int c;
+unsigned int a, b;		//unsigned int is variable declaration of integer type without any sign
+int c;					//variable declaration for integer type		
 printf("Enter the two unsigned integers:\n");
 scanf("%d%d",&a,&b);
-c = a & b;
+c = a & b; /* Converts a and b into binary then perform anding*/
 printf("%d & %d = %d\n", a, b, c );
 return 0;
 }
