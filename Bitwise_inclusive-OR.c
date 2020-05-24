a6042a04c5e336129cebaac453727a6258f09297
Sailesh Singh <sailesh36687@gmail.com>
2020-05-25 00:12:46 +0545

    Program code "Program to inclusively OR two unsigned integers" complete.

---
+++ Bitwise_inclusive-OR.c
@@ -0,0 +1,12 @@
+//Program to inclusively OR two unsigned integers
+#include <stdio.h>		
+int main(void)			
+{
+unsigned int a, b;		
+int c;							
+printf("Enter the two unsigned integers:\n");
+scanf("%d%d",&a,&b);
+c = a | b; 
+printf("%d | %d = %d\n", a, b, c );
+return 0;
+}
