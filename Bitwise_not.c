2cc49c79ddd3534474495ec94bd9c0626d2c8fbb
Sailesh Singh <sailesh36687@gmail.com>
2020-05-25 01:06:59 +0545

    Inline comments to understand the each steps of program.

--- Bitwise_not.c
+++ Bitwise_not.c
@@ -1,12 +1,12 @@
 //Program find bitwise not or one's complement of unsigned integer
-#include <stdio.h>
-int main(void)			
+#include <stdio.h>		//Include basic input and output statements		
+int main(void)			//int in the main function is return type	
 {
-unsigned int a;	
-int c;											
+unsigned int a;			//unsigned int is variable declaration of integer type without any sign	
+int c;					//variable declaration for integer type												
 printf("Enter a unsigned integers:\n");
 scanf("%d",&a);
-c = ~a;  
+c = ~a;  /* Converts a and b into binary then perform inclusive OR*/  
 printf("~%d = %d\n", a, c );
 return 0;
 }
