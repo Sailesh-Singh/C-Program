078dcacaa25c7383c252476ec685f6b30b7dd9b4
Sailesh Singh <sailesh36687@gmail.com>
2020-05-25 00:17:26 +0545

    Inline comments to understand the each steps of program.

--- Bitwise_inclusive-OR.c
+++ Bitwise_inclusive-OR.c
@@ -1,12 +1,12 @@
 //Program to inclusively OR two unsigned integers
-#include <stdio.h>		
-int main(void)			
+#include <stdio.h>		//Include basic input and output statements		
+int main(void)			//int in the main function is return type	
 {
-unsigned int a, b;		
-int c;							
+unsigned int a, b;		//unsigned int is variable declaration of integer type without any sign	
+int c;					//variable declaration for integer type								
 printf("Enter the two unsigned integers:\n");
 scanf("%d%d",&a,&b);
-c = a | b; 
+c = a | b;  /* Converts a and b into binary then perform inclusive OR*/
 printf("%d | %d = %d\n", a, b, c );
 return 0;
 }
