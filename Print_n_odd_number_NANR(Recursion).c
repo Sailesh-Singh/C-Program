85cef9d371445d9c6d712096d77308c76b761c67
Sailesh Singh <sailesh36687@gmail.com>
2020-05-31 17:31:22 +0545

    Multiline comment in the top of code for author details and contact links with date of creation

--- Print_n_odd_number_NANR(Recursion).c
+++ Print_n_odd_number_NANR(Recursion).c
@@ -1,13 +1,20 @@
+/*This Program is coded by Mr. Sailesh Singh
+You can follow and look after the work done by me on following links
+Github:	  https://github.com/Sailesh-Singh
+linkedin: https://www.linkedin.com/in/saileshsingh36
+Date of creation:5/31/2020
+Please use this for learning purpose only.
+*/
 //Program to print 'n' odd number with NANR(no argument and no return)
 #include<stdio.h>		//Include basic input and output statements
 void odd()
 {
    int i=1,n, count=1;	//variable declaration for integer type
-    printf("Enter n:");
+    printf("Enter number of terms:");
     scanf("%d",&n);
     while (i<=n)		//checking while condition
     {
-        printf("%d\t",count);
+        printf("%d\n",count);
         count=count+2;	//Adding 2 to count to make succeding odd number
         i++;			//counts number of terms
     }
