b8f1274079de511e64f63f6905752692c20562d1
Sailesh Singh <sailesh36687@gmail.com>
2020-05-31 20:27:53 +0545

    Inline comments to understand the each steps of program.

--- Print_n_even_number_NANR(Recursion).c
+++ Print_n_even_number_NANR(Recursion).c
@@ -1,18 +1,19 @@
 //Program to print 'n' even number with NANR(no argument and no return)
 #include<stdio.h>
+#include<stdio.h>		//Include basic input and output statements
 void even()
 {
-   int i=0,n, count=0;
-    printf("Enter n:");
+   int i=0,n, count=0;	//variable declaration for integer type
+    printf("Enter number of terms:");
     scanf("%d",&n);
-    while (i<=n)
+    while (i<=n)		//checking while condition
     {
-        printf("%d\t",count);
-        count=count+2;
-        i++;
+        printf("%d\n",count);
+        count=count+2;	//Adding 2 to count to make succeding even number
+        i++;			//counts number of terms
     }
 }
-void main()
+void main() 	   		//void in the main function is non-return type
 {
-    even();
+    even();				//calling even() sub-function
 }
