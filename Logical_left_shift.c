7d9bf81fee9aae0042f0a5108608f3b9c1379cb7
Sailesh Singh <sailesh36687@gmail.com>
2020-05-31 15:44:50 +0545

    Inline comments to understand the each steps of program.

--- Logical_left_shift.c
+++ Logical_left_shift.c
@@ -1,14 +1,14 @@
 //Program to left-shift an integer by n digit
-#include <stdio.h>		
-int main(void)			
+#include <stdio.h>		//Include basic input and output statements
+int main(void)			//int in the main function is return type
 {
-unsigned int a;	
-int c,n;				
+unsigned int a;			//unsigned int is variable declaration of integer type without any sign
+int c,n;				//variable declaration for integer type		
 printf("Enter the unsigned integer:\n");
-scanf("%d",&a);
+scanf("%d",&a);			//scaning integer for shifting
 printf("Enter the number by which %d should be shifted:\n",a);
-scanf("%d",&n);
-c = a << n;
-printf("%d << %d = %d\n",a,n,c);
+scanf("%d",&n);			//scaning a integer by which integer is shifted 
+c = a << n;				//condition for shifiting 'a' integer by 'n' digit in right side
+printf("%d << %d = %d\n",a,n,c);//printing the result
 return 0;
 }
