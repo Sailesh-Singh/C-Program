787b02d9eff57f10a3191474bdb8be4c49b6f207
Sailesh Singh <sailesh36687@gmail.com>
2020-06-01 22:42:01 +0545

    Inline comments to understand the each steps of program

--- Greatest_number_NANR(Recursion).c
+++ Greatest_number_NANR(Recursion).c
@@ -1,17 +1,18 @@
 //Program to print the largest number using NANR(No argument and with no-return type)
-#include<stdio.h>
-void greater()
+#include<stdio.h>		//Include basic input and output statements
+void greater()			//Intialilzing sub-function greater()
 {
-	int a,b,largest;
+	int a,b,largest;	//variable declaration for integer type
 	printf("Enter two numbers:\n");
 	scanf("%d%d",&a,&b);
+	/*checking the condition using if_else*/
 	if(a>b)
 		largest=a;
 	else
 		largest=b;
-	printf("%d is the greater",largest);
+	printf("%d is the greater",largest); // Printing the result value
 }
-void main()
+void main()			// void in main function is non-return type
 {
-	greater();
+	greater(); //calling sub-fuunction
 }
