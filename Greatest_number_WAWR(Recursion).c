dcd3e3dcaecf5443277646e649984a9639efd782
Sailesh Singh <sailesh36687@gmail.com>
2020-06-01 23:14:40 +0545

    Inline comments to understand the each steps of program

--- Greatest_number_WAWR(Recursion).c
+++ Greatest_number_WAWR(Recursion).c
@@ -1,20 +1,21 @@
 //Program to print the largest number using WAWR(with argument and with return type)
-#include<stdio.h>
-int greater(int a,int b)
+#include<stdio.h>			//Include basic input and output statements
+int greater(int a,int b)	//initializing sub-function with arguments
 {
-	int largest=0;
+	int largest;    		//variable declaration for integer type
+/*checking the condition using if_else*/	
 	if(a>b)
 	largest=a;
 	else
 	largest=b;
-	return largest;
+	return largest;			//Returning the true condition value to main function
 }
-int main()
+int main()					//int in the main function is return type
 {
-	int x,y,result;
+	int x,y,result;    		//variable declaration for integer type
 	printf("Enter the two numbers:\n");
 	scanf("%d%d",&x,&y);
-	result=greater(x,y);
-	printf("%d is the greater\n",result);
+	result=greater(x,y);	//calling sub-fuunction and storing its return value to result
+	printf("%d is the greater\n",result);	//Printing the result
 	return 0;
 }
