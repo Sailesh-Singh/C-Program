bc7cb60cd64581dfd124bf3500693c3b207cbb18
Sailesh Singh <sailesh36687@gmail.com>
2020-06-02 00:12:54 +0545

    Inline comments to understand the each steps of program

--- Print_n_natural_number_NANR.c
+++ Print_n_natural_number_NANR.c
@@ -1,14 +1,15 @@
 //Program to print the natural number using NANR(No argument and with no-return type)
-#include<stdio.h>
-void naturalnum()
+#include<stdio.h>		//Include basic input and output statements
+void naturalnum()		//Intialilzing sub-function naturalnum()
 {
-	int i,n;
+	int i,n;			//variable declaration for integer type
 	printf("Enter the no. of terms:  ");
 	scanf("%d",&n);
+/*Iterating and checking the validity of condition using for_loop*/
 	for(i=1; i<=n;i++)
-	printf("\n%d",i);
+	printf("\n%d",i); 	// Printing the result value
 }
-void main()
+void main()				// void in main function is non-return type
 {
-	naturalnum();
+	naturalnum();		//calling sub-fuunction
 }
