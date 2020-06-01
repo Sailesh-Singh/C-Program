80b54eedee3b50831a6e93100069664a75f1028b
Sailesh Singh <sailesh36687@gmail.com>
2020-06-02 01:32:29 +0545

    Inline comments to understand the each steps of program

--- Odd_and_even_number_WANR.c
+++ Odd_and_even_number_WANR.c
@@ -1,14 +1,13 @@
 //Program to check wether number is odd or even using NANR(No argument and with no-return type)
-#include<stdio.h>
-	void even(int a)
-	{
-	int result;
-	result%2==0?printf("Its even"):printf("Its odd");
+#include<stdio.h>			//Include basic input and output statements
+	void even(int a)		//Intialilzing sub-function even() 
+	int result;				//variable declaration for integer type
+	result%2==0?printf("Its even"):printf("Its odd");//using ternary/conditional operator to validate the result
 }
-void main()
+void main()					// void in main function is non-return type
 {
-	int a;
+	int a;					//variable declaration for integer type
 	printf("Enter the number to be checked: ");
 	scanf("%d",&a);
-	even(a);
+	even(a);			//calling sub-fuunction
 }
