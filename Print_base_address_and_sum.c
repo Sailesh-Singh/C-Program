6b82f050750374ca6a6434b3280ee4ba4b0c110f
Sailesh Singh <sailesh36687@gmail.com>
2020-06-08 11:08:10 +0545

    Inline comment to understand the steps of program

--- Print_base_address_and_sum.c
+++ Print_base_address_and_sum.c
@@ -1,15 +1,17 @@
 //Program to print the base address of elements in array and its sum
-#include<stdio.h>
-void main()
+#include<stdio.h>			//Include basic input and output statements
+void main() 		    	//void in the main function is non-return type
 {
-	int *ptr, sum=0, i;
-	int a[5]={2,3,4,5,6};
-	ptr=&a;
+	int *ptr, sum=0, i;		//variable declaration for integer type and *ptr is a pointer pointing the allocated memory location
+	int a[5]={2,3,4,5,6};	//variable declaration for integer type, declaring the elements of array
+	ptr=&a;					//Assigning the allocated memory location of 'a' in 'ptr'
+	/*ptr=&a[0]; //Instead of line 7, this can also be used*/
+	/*For loop used to iterate the statements for 5 times*/
 	for(i=0;i<5;i++)
 	{
-	    printf("The base address is %p\n",ptr);
-	    sum=sum+*ptr;
-	    ptr++;
+	    printf("The base address is %p\n",ptr);	//'%p' is used to print the base address of the elements
+	    sum=sum+*ptr;		// add the elements and store its value to variable 'sum'
+	    ptr++;			 
 	}
-	printf("The sum of numbers is %d",sum);
+	printf("The sum of numbers is %d",sum); 	//printing the total sum of all the elements within the array
 }
