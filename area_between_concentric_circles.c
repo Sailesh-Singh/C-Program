9c2102e5fc4d6818f239c89a97d90cf8b9b1b621
vinit-44code <vinitqwwr@gmail.com>
2026-01-18 21:09:12 +0530

    i have added a new libari maths.h , and used pow()

--- area_between_concentric_circles.c
+++ area_between_concentric_circles.c
@@ -6,13 +6,14 @@ Date of creation:3/26/2020
 Please use this for learning purpose only.
 */
 //Program to calculate the area between two concentric circle (circle having same centre but different radii)
-#include<stdio.h>		//Include basic input and output statements
+#include<stdio.h>	
+#include<math.h>	//Include basic input and output statements
 void main()			    //void in the main function is non-return type
 {
 	float r,R,a,A;  	//variable declaration for float type
 	printf("Enter the first and second radii");
 	scanf("%f",&r,&R);
-	a=3.14*r*r;				//Area of small circle
+	a=3.14*pow(r,2);				//Area of small circle
 	A=3.14*R*R;				//Area of the big circle
 	printf("The area between the concentric circle is %f and %f",a,A); //Printing the result
 }
